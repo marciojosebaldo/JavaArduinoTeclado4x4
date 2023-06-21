07452886d02c7991309551635d6ee0b764b44219
marciojosebaldo <marciojosebaldo@gmail.com>
2023-06-21 20:09:18 -0300

    Add files via upload

---
+++ JavaArduino/src/main/java/JavaArduinoSpring/JavaArduino/JavaArduinoApplication.java
@@ -0,0 +1,13 @@
+package JavaArduinoSpring.JavaArduino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaArduinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaArduinoApplication.class, args);
+	}
+
+}
---
+++ JavaArduino/src/test/java/JavaArduinoSpring/JavaArduino/JavaArduinoApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaArduinoSpring.JavaArduino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaArduinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
